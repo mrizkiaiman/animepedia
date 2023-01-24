e9f77784108bdb2f414e0dbb94db3ab04db640d5
mrizkiaiman <rizki.aiman@arvis.id>
2023-01-24 18:11:49 +0700

    remove mal api

--- services/main/api.ts
+++ services/main/api.ts
@@ -10,14 +10,3 @@ export const getListAnime = (page: number): Promise<APIResponse<Anime[]>> => {
 export const getAnimeDetail = (id: string): Promise<APIResponse<Anime>> => {
   return GET(`${process.env.NEXT_PUBLIC_API_URL}/anime/${id}`)
 }
-
-export const getListAnimeFromMAL = (): Promise<APIResponse<Anime[]>> => {
-  return GET(`${process.env.NEXT_PUBLIC_API_URL_MAL}/anime/ranking?limit=100`, {
-    'X-MAL-CLIENT-ID': process.env.NEXT_PUBLIC_API_URL_MAL_CLIENTID,
-    'content-type': 'application/json; charset=UTF-8',
-  })
-}
-
-export const getAnimeDetailFromMAL = (id: string): Promise<APIResponse<Anime>> => {
-  return GET(`${process.env.NEXT_PUBLIC_API_URL_MAL}/anime/${id}`, { 'X-MAL-CLIENT-ID': process.env.NEXT_PUBLIC_API_URL_MAL_CLIENTID })
-}
--- services/main/hooks.ts
+++ services/main/hooks.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { getAnimeDetail, getListAnime, getAnimeDetailFromMAL, getListAnimeFromMAL } from '@app/services/main/api'
+import { getAnimeDetail, getListAnime } from '@app/services/main/api'
 import { animeKeys } from '@app/services/queryKeys'
 import { AxiosError } from 'axios'
 import { Anime } from '@app/types/jikanAPI/anime'
@@ -13,18 +13,6 @@ export const useAnimeDetail = (id: string) => {
   return useQuery<APIResponse<Anime>, AxiosError>({
     queryKey: animeKeys.detail(id),
     queryFn: () => getAnimeDetail(id),
-    enabled: !!id,
-  })
-}
-
-export const useAnimeListMAL = () => {
-  return useQuery<APIResponse<Anime[]>, AxiosError>(animeKeys.mal(), () => getListAnimeFromMAL())
-}
-
-export const useAnimeDetailMAL = (id: string) => {
-  return useQuery<APIResponse<Anime>, AxiosError>({
-    queryKey: animeKeys.detailMAL(id),
-    queryFn: () => getAnimeDetailFromMAL(id),
     enabled: !!id,
   })
 }
